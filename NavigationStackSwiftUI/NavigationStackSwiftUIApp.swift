6e348cc9c495252957a1c6fc278284c9a7d1dea3
Sandy Gomez <sgomez1029@gmail.com>
2024-04-30 14:06:10 -0700

    Initial Commit

---
+++ NavigationStackSwiftUI/NavigationStackSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackSwiftUIApp.swift
+//  NavigationStackSwiftUI
+//
+//  Created by Sandra Gomez on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
